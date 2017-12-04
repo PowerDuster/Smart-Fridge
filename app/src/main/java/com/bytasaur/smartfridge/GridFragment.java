0e1dc0dde5415f22545c3ddac4c95e2d7625a44e
Barksalot <herstalf@gmail.com>
2017-12-04 20:15:16 +0500

    rearranged code

--- app/src/main/java/com/bytasaur/smartfridge/GridFragment.java
+++ app/src/main/java/com/bytasaur/smartfridge/GridFragment.java
@@ -1,6 +1,7 @@
 package com.bytasaur.smartfridge;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +17,7 @@ public class GridFragment extends android.support.v4.app.Fragment {
     Toast toast;
 
     @Override
-    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_grid, container, false);
         GridView gridView=v.findViewById(R.id.grid);
         gridView.setAdapter(MainActivity.adapter);
